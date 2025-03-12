972bce15d0a7ba35b57a41de0e655d52d93a5529
andreyfesunov <fsnv04@gmail.com>
2025-03-12 13:56:14 +0300

    fix: android build

--- android/app/src/main/kotlin/com/example/flutter_wysiwyg_editor_comparison/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_wysiwyg_editor_comparison/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.flutter_wysiwyg_editor_comparison
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
